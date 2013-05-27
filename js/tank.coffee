82d10bc184c3ab2519e14c8ab1ac1fa5d44d16ae
Yarkeev Denis <d.yarkeev@corp.mail.ru>
2013-05-27 14:36:24 +0400

    *

--- js/tank.coffee
+++ js/tank.coffee
@@ -500,7 +500,6 @@
 				@$bullet.addClass 'hole'
 			, @_explodeTime
 			scrollTop = $(window).scrollTop()
-			console.log @position.left, @position.top + scrollTop
 			el = document.elementFromPoint(@position.left, @position.top)
 			if el != @_$domContainer.get(0)
 				@findExplodeElement($(el)).fadeOut TIME_OF_DESTROY_ELEMENT
--- js/tank.js
+++ js/tank.js
@@ -620,7 +620,6 @@ var __hasProp = {}.hasOwnProperty,
         return _this.$bullet.addClass('hole');
       }, this._explodeTime);
       scrollTop = $(window).scrollTop();
-      console.log(this.position.left, this.position.top + scrollTop);
       el = document.elementFromPoint(this.position.left, this.position.top);
       if (el !== this._$domContainer.get(0)) {
         return this.findExplodeElement($(el)).fadeOut(TIME_OF_DESTROY_ELEMENT);
